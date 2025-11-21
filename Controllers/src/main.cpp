79b2eaaaa0b43b3587ebe0fc3093944bc46f0426
Maddie Bell-Leemen <maddie_bl@protonmail.com>
2025-11-21 12:34:07 +1100

    Added a directory that'll house the tools I build for interfacing with stuff

---
+++ Controllers/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <openmvrpc.h>
+void setup() {
+    Serial.begin(9600); // Output to connected computer
+    Serial.println("Well Hey; didn't see ya there :3");
+
+}
+
+void loop() {
+    Serial.println("hiiii ^•^");
+}
